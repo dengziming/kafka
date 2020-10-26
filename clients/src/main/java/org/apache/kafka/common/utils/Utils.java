41fb1206560fe2c012b112659422d10336838d0e
dengziming <dengziming@growingio.com>
2020-10-26 14:02:34 +0800

    MINOR: replace for loop with bulk load function

--- clients/src/main/java/org/apache/kafka/common/utils/Utils.java
+++ clients/src/main/java/org/apache/kafka/common/utils/Utils.java
@@ -724,8 +724,7 @@ public final class Utils {
     @SafeVarargs
     public static <T> Set<T> mkSet(T... elems) {
         Set<T> result = new HashSet<>((int) (elems.length / 0.75) + 1);
-        for (T elem : elems)
-            result.add(elem);
+        result.addAll(Arrays.asList(elems));
         return result;
     }
 
@@ -737,10 +736,7 @@ public final class Utils {
      */
     @SafeVarargs
     public static <T extends Comparable<T>> SortedSet<T> mkSortedSet(T... elems) {
-        SortedSet<T> result = new TreeSet<>();
-        for (T elem : elems)
-            result.add(elem);
-        return result;
+        return new TreeSet<>(Arrays.asList(elems));
     }
 
     /**
